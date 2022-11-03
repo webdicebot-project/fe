d4490eda9a93748ce39408f506cc24d66f8dabb3
mhqb365 <mhqb365@gmail.com>
2022-11-03 08:42:15 +0700

    :rocket:

--- src/views/_beta.js
+++ src/views/_beta.js
@@ -7,19 +7,19 @@ const beta = [
     label: 'bc.game - classic dice',
     value: `const CASINO_GAME = 'bc-classic-dice';`,
     tips: `Paste installer script at game page\n
-  example: bc.game/game/classic-dice`,
+example: bc.game/game/classic-dice`,
   },
   {
     label: 'bc.game - hash dice',
     value: `const CASINO_GAME = 'bc-hash-dice';`,
     tips: `Paste installer script at game page\n
-  example: bc.game/game/hash-dice`,
+example: bc.game/game/hash-dice`,
   },
   {
     label: 'bc.game - limbo',
     value: `const CASINO_GAME = 'bc-limbo';`,
     tips: `Paste installer script at game page\n
-  example: bc.game/game/limbo`,
+example: bc.game/game/limbo`,
   },
   {
     label: 'bch.games - dice',
@@ -29,19 +29,19 @@ const beta = [
     label: 'betfury.io - dice',
     value: `const CASINO_GAME = 'betfury-dice';`,
     tips: `Paste installer script at home page\n
-  example: betfury.io`,
+example: betfury.io`,
   },
   {
     label: 'betfury.io - limbo',
     value: `const CASINO_GAME = 'betfury-limbo';`,
     tips: `Paste installer script at home page\n
-  example: betfury.io`,
+example: betfury.io`,
   },
   {
     label: 'betfury.io - space dice',
     value: `const CASINO_GAME = 'betfury-space-dice';`,
     tips: `Paste installer script at home page\n
-  example: betfury.io`,
+example: betfury.io`,
   },
   {
     label: 'bitsler.com - boom',
@@ -119,13 +119,13 @@ const beta = [
     label: 'megaplay.io - dice',
     value: `const CASINO_GAME = 'megaplay-dice';`,
     tips: `Paste installer script at home page\n
-  example: megaplay.io`,
+example: megaplay.io`,
   },
   {
     label: 'megaplay.io - limbo',
     value: `const CASINO_GAME = 'megaplay-limbo';`,
     tips: `Paste installer script at home page\n
-  example: megaplay.io`,
+example: megaplay.io`,
   },
   {
     label: 'nanogames.io - classic dice',
@@ -159,13 +159,13 @@ const beta = [
     label: 'punt.com - dice',
     value: `const CASINO_GAME = 'punt-dice';`,
     tips: `Paste installer script at page without load bet\n
-  example: punt.com/casino/group/originals/`,
+example: punt.com/casino/group/originals/`,
   },
   {
     label: 'punt.com - limbo',
     value: `const CASINO_GAME = 'punt-limbo';`,
     tips: `Paste installer script at page without load bet\n
-  example: punt.com/casino/group/originals/`,
+example: punt.com/casino/group/originals/`,
   },
   {
     label: 'sicodice.com',
@@ -175,13 +175,13 @@ const beta = [
     label: 'stake.com - dice',
     value: `const CASINO_GAME = 'stake-dice';`,
     tips: `Paste installer script at page without load bet\n
-  example: stake.com/transactions/deposits`,
+example: stake.com/transactions/deposits`,
   },
   {
     label: 'stake.com - limbo',
     value: `const CASINO_GAME = 'stake-limbo';`,
     tips: `Paste installer script at page without load bet\n
-  example: stake.com/transactions/deposits`,
+example: stake.com/transactions/deposits`,
   },
   {
     label: 'starbets - dice',

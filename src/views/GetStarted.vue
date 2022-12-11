f52f2e69c0b29ace6237471000c6c5be6ea7b52b
mhqb365 <mhqb365@gmail.com>
2022-12-11 21:16:56 +0700

    add-video

--- src/views/GetStarted.vue
+++ src/views/GetStarted.vue
@@ -1,21 +1,19 @@
 <template>
   <div>
     <CAlert color="danger">
-      <strong>
-        <p>
-          Gambling is gambling no matter what you do or how good your strategy
-          is. The dealer always wins if you keep playing. Winners always know
-          when to stop
-        </p>
+      <p>
+        ⚠️ Gambling is gambling no matter what you do or how good your strategy
+        is. The dealer always wins if you keep playing. Winners always know
+        when to stop
+      </p>
 
-        <p>
-          Like any human, I make mistakes, and like any program, the bot is
-          bound to have a few bugs. Use the bot at your own risk. Should you
-          come accross a bug, please report it with as much detail as possible
-          to
-          <a href="https://t.me/webdicebotgroup" target="_blank">Web DiceBot's Group</a>
-        </p>
-      </strong>
+      <p>
+        ⚠️ Like any human, many mistakes, and like any program, the bot is
+        bound to have a few bugs. Use the bot at your own risk. Should you
+        come accross a bug, please report it with as much detail as possible
+        to
+        <a href="https://t.me/webdicebotgroup" target="_blank">Web DiceBot's Group</a>
+      </p>
     </CAlert>
 
     <CCard class="mb-4">
@@ -28,19 +26,19 @@
           <div class="row text-center">
             <div class="col-sm-4">
               <h1><i class="fa-solid fa-robot text-primary"></i></h1>
-              <h4>Make Interface</h4>
+              <h3>Make Interface</h3>
               <p>
                 Bot interface maked directly on dice sites after you install
               </p>
             </div>
             <div class="col-sm-4">
               <h1><i class="fa-solid fa-code text-warning"></i></h1>
-              <h4>Read Script</h4>
+              <h3>Read Script</h3>
               <p>Bot will read your dice script (LUA/JS) on script box</p>
             </div>
             <div class="col-sm-4">
               <h1><i class="fa-solid fa-play text-success"></i></h1>
-              <h4>Auto Betting</h4>
+              <h3>Auto Betting</h3>
               <p>
                 Betting base on dice script, earning or losing depend on your
                 script
@@ -51,58 +49,6 @@
       </CCardBody>
     </CCard>
 
-    <CRow>
-      <CCol md="6">
-        <CCard class="mb-4">
-          <CCardBody>
-            <h4>
-              <strong>Dice script in LUA </strong>
-            </h4>
-
-            <pre>
-// example LUA code
-chance=49.5
-bethigh=true
-basebet=0.00000001
-nextbet=basebet
-
-function dobet()
-  if (win) then
-    nextbet=basebet
-  else
-    nextbet=previousbet*2
-  end
-end</pre>
-          </CCardBody>
-        </CCard>
-      </CCol>
-
-      <CCol md="6">
-        <CCard class="mb-4">
-          <CCardBody>
-            <h4>
-              <strong>Dice script in JS</strong>
-            </h4>
-
-            <pre>
-// example JS code
-chance=49.5
-bethigh=true
-basebet=0.00000001
-nextbet=basebet
-
-function dobet() {
-  if (win) {
-    nextbet=basebet
-  } else {
-    nextbet=previousbet*2
-  }
-}</pre>
-          </CCardBody>
-        </CCard>
-      </CCol>
-    </CRow>
-
     <CCard class="mb-4">
       <CCardBody>
         <h4>
@@ -110,7 +56,7 @@ function dobet() {
         </h4>
 
         <p>
-          The following user agreement is to read in its entirety prior to the
+          👉 The following user agreement is to read in its entirety prior to the
           use of Web DiceBot's products and services. The agreement constitutes
           a legal binding between the user and all our services
         </p>
@@ -131,31 +77,20 @@ function dobet() {
           <strong>How to use? </strong>
         </h4>
 
+        <video controls :src="$options.video" class="border border-secondary w-100"></video>
+
         <p>
-          Suggest browser for best performance: FireFox, Chrome, Microsoft Edge,
+          👉 Browser for best performance: FireFox, Chrome, Microsoft Edge,
           Brave, Coc Coc, Kiwi Browser (Android)
           <br />
-          Suggest dice scripts:
-          <a href="https://github.com/webdicebot-project/awesome-scripts" target="_blank">
-            webdicebot-project/awesome-scripts
-          </a>
-        </p>
-
-        <pre>1. <router-link to="/pages/login">Login</router-link> to Web DiceBot system, deposit Tron (TRX) to <router-link to="/my-wallet">My Wallet</router-link> then <router-link to="/buy-license">Buy License</router-link> with a days you want (minimum 10 days)
-2. Copy installer script at <router-link to="/bot-installer">Bot Installer</router-link>
-3. Go to casino sites then open Developer tools (press the F12 key) and select Console tab
-4. Paste installer script into Console and wait bot interface create
-5. Play with your dice script when the bot interface has finished loading</pre>
-
-        <p>
-          Your dice script must clean, please follow syntax of programming
+          👉 Your dice script must clean, please follow syntax of programming
           language
           <br />
-          You must follow syntax of
+          👉 You must follow syntax of
           <a href="https://lua.org/manual/5.3" target="_blank"> Lua v5.3 </a>
           when you use Lua script
           <br />
-          Use
+          👉 Use
           <a href="https://www.jdoodle.com/execute-lua-online/" target="_blank">JDoodle.com</a>
           to check clean your dice script
         </p>
@@ -169,90 +104,65 @@ function dobet() {
         </h4>
 
         <p>
-          <a href="https://bc.game/i-1fz8gf65-n" target="_blank"> bc.game </a>
-          <br />
-          <a href="https://bch.games/play/UT7QfUO3" target="_blank"> bch.games </a>
-          <br />
+          <a href="https://bc.game/i-1fz8gf65-n" target="_blank"> bc.game </a>,
+          <a href="https://bch.games/play/UT7QfUO3" target="_blank"> bch.games </a>,
           <a href="https://betfury.io/?r=5f86fffc38b5653094d3206b" target="_blank">
             betfury.io
-          </a>
-          <br />
+          </a>,
           <a href="https://bitsler.com/?ref=mhqb365" target="_blank">
             bitsler.com
-          </a>
-          <br />
+          </a>,
           <a href="https://bitvest.io/?r=134231" target="_blank">
             bitvest.io
-          </a>
-          <br />
+          </a>,
           <a href="https://casinoroyale.bet" target="_blank">
             casinoroyale.bet
-          </a>
-          <br />
-          <a href="https://crypto.games" target="_blank"> crypto.games </a>
-          <br />
+          </a>,
+          <a href="https://crypto.games" target="_blank"> crypto.games </a>,
           <a href="https://cryptoplay.io/?c=mhqb365" target="_blank">
             cryptoplay.io
-          </a>
-          <br />
-          <a href="https://duckdice.io" target="_blank"> duckdice.io </a>
-          <br />
+          </a>,
+          <a href="https://duckdice.io" target="_blank"> duckdice.io </a>,
           <a href="https://faucetpay.io/?r=2102948" target="_blank">
             faucetpay.io
-          </a>
-          <br />
-          <a href="https://freebitco.in" target="_blank"> freebitco.in </a>
-          <br />
+          </a>,
+          <a href="https://freebitco.in" target="_blank"> freebitco.in </a>,
           <a href="https://jacksclub.io/?r=~mhqb365" target="_blank">
             jacksclub.io
-          </a>
-          <br />
+          </a>,
           <a href="https://luckydiamond.io/r/mhqb365" target="_blank">
             luckydiamond.io
-          </a>
-          <br />
+          </a>,
           <a href="https://megaplay.io/?c=ZNae4ChvlSSADkGsIxFf" target="_blank">
             megaplay.io
-          </a>
-          <br />
+          </a>,
           <a href="https://nanogames.io/i-f0810ew-n/" target="_blank">
             nanogames.io
-          </a>
-          <br />
+          </a>,
           <a href="https://paradice.in/?c=@mhqb365" target="_blank">
             paradice.in
-          </a>
-          <br />
+          </a>,
           <a href="https://pasino.com/?user_id=41111" target="_blank">
             pasino.com
-          </a>
-          <br />
+          </a>,
           <a href="https://primedice.com/?c=3e6d33fce7" target="_blank">
             primedice.com
-          </a>
-          <br />
-          <a href="https://punt.com/?aid=1949" target="_blank"> punt.com </a>
-          <br />
-          <a href="https://sicodice.com" target="_blank"> sicodice.com </a>
-          <br />
+          </a>,
+          <a href="https://punt.com/?aid=1949" target="_blank"> punt.com </a>,
+          <a href="https://sicodice.com" target="_blank"> sicodice.com </a>,
           <a href="https://stake.com/?c=26b21743fa" target="_blank">
             stake.com
-          </a>
-          <br />
+          </a>,
           <a href="https://starbets.io/ref=r3oaasMDgA" target="_blank">
             starbets.io
-          </a>
-          <br />
+          </a>,
           <a href="https://windice.io/?r=mhqb365" target="_blank">
             windice.io
-          </a>
-          <br />
+          </a>,
           <a href="https://wintomato.com/en/registration/1604" target="_blank">
             wintomato.com
-          </a>
-          <br />
+          </a>,
           <a href="https://wolf.bet/?c=mhqb365" target="_blank"> wolf.bet </a>
-          <br />
         </p>
       </CCardBody>
     </CCard>
@@ -260,7 +170,9 @@ function dobet() {
 </template>
 
 <script>
+import video from '@/assets/webdicebot.mp4'
 export default {
+  video,
   data() {
     return {
       urlTelegramBot:
--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -3,7 +3,7 @@
     <div class="jumbotron jumbotron-fluid mt-5 p-5">
       <div class="container mt-5">
         <div class="row">
-          <div class="col-md-4">
+          <div class="col-md-4 p-2">
             <h2 class="text-warning">
               <strong class="display-4"> Web DiceBot </strong>
             </h2>
@@ -12,21 +12,29 @@
               Automate betting with programmer mode directly on dice sites
             </p>
 
-            <p class="d-inline">
-              <router-link to="/get-started" class="mt-4 mb-2">
+            <p>
+              Pricing 0.2$ per day
+            </p>
+
+            <div class="d-inline">
+              <router-link to="/get-started">
                 <button type="button" class="btn btn-warning btn-lg">
                   Get Started
                 </button>
               </router-link>
-              with 0.2$ per day
-            </p>
+            </div>
           </div>
-          <div class="col-md-8 d-none d-md-block">
+
+          <div class="col-md-8 p-2">
             <img :src="$options.photo" alt="Web DiceBot"
               class="w-100 rounded shadow-lg animate__animated animate__zoomIn" />
           </div>
         </div>
       </div>
+    </div>
+
+    <div class="fixed-bottom bg-dark text-center text-white">
+      Web DiceBot &copy; 2019
     </div>
   </div>
 </template>
